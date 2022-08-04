021eb368e781e3b900d3c08f0f6381d578bc916b
Alex <alzombii@gmail.com>
2022-08-04 14:01:40 +0300

    Work started

---
+++ ServiceExercise/MyService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ServiceExercise
+{
+    public class MyService : IService
+    {
+        public int getSummary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void sendRequest(Request request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
